12e7a360b501d479668c444217dd555a250c64af
Jônatas Lacerda <jonatasflacerda@hotmail.com>
2013-04-02 10:49:02 -0300

    update 04.02.13

--- baseflow.f90
+++ baseflow.f90
@@ -1,14 +1,14 @@
-program mixing-layer
+program baseflow
 
 ! Program to generate baseflow of a 2D compressible mixing layer
 ! This code is a extension of code from Leonardo da Costa Salemi (LCP/INPE)
 ! and Márcio Teixeira de Mendonça (CTA/IAE)
-
-!     This code solves the compressible binary shear layer equations transformed
+!
+!     This code soluves the compressible binary shear layer equations transformed
 !    according to Lees-Dorodnitsyn Transformation (Anderson, Hypersonic 
 !     and High Temperature Gas Dynamics).
 ! 
-!     The code solves the equations for Chi,Pr,Le =1 and Chi,Pr,Le = constant.
+!     The code soluves the equations for Chi,Pr,Le =1 and Chi,Pr,Le = constant.
 !
 !     Velocity, Enthalpy and Mass Fraction profiles: 
 !
@@ -18,55 +18,28 @@ program mixing-layer
 ! Jonatas F Lacerda    jonatasflacerda.hotmail.com
 ! ICMC - USP
 
-! date: 19.03.2013
+! date: 04.02.2013
 
 implicit none
 include '../var.f90'
 include 'comm.mshbase'
 include 'comm.varbase'
 
- ! Mesh generation
+ !........................ Mesh generation
  call mesh_baseflow
  
- ! Read input data from coupled.in
- open(unit=1,file='coupled.in',status='UNKNOWN')
-  read(1,*)
-  read(1,*)
-  read(1,*)eps
-  read(1,*)h1,hmin
-  read(1,*)
-  read(1,*)
-  read(1,*)L,R
-  read(1,*)
-  read(1,*)
-  read(1,*)P,Q
-  read(1,*)
-  read(1,*)
-  read(1,*)W,Z
-  read(1,*)
-  read(1,*)
-  read(1,*)press
-  read(1,*)T(kmax),u(kmax)      ! Fast side data
-  read(1,*)T(1),u(1)            ! Slow side data
-  read(1,*)
-  read(1,*)
-  read(1,*)ngas1
-  read(1,*)
-  read(1,*)
-  read(1,*)ngas2
-  read(1,*)
-  read(1,*)
-  read(1,*)const
-  read(1,*)
-  read(1,*)
-  read(1,*)xpos
- close(1)
-	  
+ !........................ Calculation of eta variable
+ call calc_eta
+ 
+ !........................ Integration of equations
+ call integra
+ 
+
  stop
 
-end program mixing-layer
+end program baseflow
 
-!!###############################################################################
+!*******************************************************************************
 
 subroutine mesh_baseflow
 
@@ -80,8 +53,9 @@ include '../var.f90'
 include 'comm.mshbase'
 
 integer(4):: icount, icountmax
-integer(4):: j, jmid, jmaxver, jmaxaux, i, istf1, istf2, ind
-real(8):: a3, eta0, eta, stxf, ep, posx, dx
+!integer(4):: j, jmid, jmaxver, jmaxaux, i, istf1, istf2, ind
+integer(4):: j, jmaxver, i, istf1, istf2, ind
+real(8):: a3, ksi0, ksi, stxf, ep, posx, dx, dy
 real(8),allocatable,dimension(:):: yaux1, yaux2
 logical:: lstop, lwrite
 
@@ -89,12 +63,70 @@ logical:: lstop, lwrite
 lwrite = .false.
 
 !******************************************************
-  ! 1) y direction calculation
+  !1) y direction calculation - uniform mesh
+  !dy = 0.15d0
+  !
+  !lstop = .false.
+  !jmaxaux =  jmax
+  !icountmax = 40
+  !icount = 1
+  !do while (.not.lstop)
+  !  ! calculating middle value of y-index: jmid
+  !  jmid = (jmaxaux+1)/2
+  !  
+  !  ! calculating index verification
+  !  jmaxver = (2*jmid)-1
+  !  
+  !  !verifying if jmid is correct
+  !  if (jmaxver.ne.jmaxaux) then
+  !    jmaxaux = jmaxaux + 1
+  !    icount = icount + 1
+  !    if (icount.gt.icountmax) then
+  !      write(*,*) 'not worked in', icountmax,' iterations'
+  !      lstop=.true.
+  !    endif
+  !  else
+  !    write(*,*) 'jmax = ', jmax, 'jmaxaux= ', jmaxaux
+  !    allocate (yaux1(jmid))
+  !    allocate (yaux2(jmaxaux))	  
+  !    lstop=.true.
+  !  endif
+  !enddo
+  !
+  !! calculating the upper part yaux1
+  !ksi = 0.d0
+  !do j = 1, jmid
+  !  yaux1(j) = ksi
+  !  ksi = ksi + dy
+  !enddo
+  !
+  !! copying yaux1 to the lower part yaux2
+  !yaux2(1) = yaux1(jmid)
+  !do j = 1,jmid-1
+  !  yaux2(j+1) = yaux1(jmid-j)
+  !enddo
+  !
+  !! rewrites yaux1 on the upper side of yaux2
+  !do j = jmid+1, jmaxaux
+  !  yaux2(j) = yaux1(j-jmid+1)
+  !enddo
+  !
+  !! calculates y on the lower side
+  !do j = 1, jmid-1
+  !  y(j) = -yaux2(j)
+  !enddo
+  !
+  !! calculates y on the upper side
+  !do j = jmid, jmax
+  !  y(j) = yaux2(j)
+  !enddo
+  
+  
+  ! 1) y direction calculation - non-uniform mesh
   lstop = .false.
   jmaxaux =  jmax
   icountmax = 40
   icount = 1
-  write(*,*) 'jmax =', jmax
   do while (.not.lstop)
     ! calculating middle value of y-index: jmid
     jmid = (jmaxaux+1)/2
@@ -106,11 +138,12 @@ lwrite = .false.
     if (jmaxver.ne.jmaxaux) then
       jmaxaux = jmaxaux + 1
       icount = icount + 1
-	  if (icount.gt.icountmax) then
+      if (icount.gt.icountmax) then
         write(*,*) 'not worked in', icountmax,' iterations'
         lstop=.true.
       endif
     else
+      write(*,*) 'jmax = ', jmax, 'jmaxaux= ', jmaxaux
       allocate (yaux1(jmid))
       allocate (yaux2(jmaxaux))	  
       lstop=.true.
@@ -119,14 +152,14 @@ lwrite = .false.
   
   !coeficients for mesh calculation (According to Babucke, 2009)
   a3 = 0.000502236538416527d0
-  eta0 =  0.d0
-
+  ksi0 =  0.d0
+  
   ! calculating the upper part yaux1
-  eta = 0.d0
-  yaux1(1) = eta
-  do j = 2, jmid
-    yaux1(j) = a3 * (eta - eta0)**3 + (eta - eta0)
-	eta = eta + dn
+  ksi = 0.d0
+  yaux1(1) = ksi
+  do j = 1, jmid
+    yaux1(j) = a3 * (ksi - ksi0)**3 + (ksi - ksi0)
+  ksi = ksi + dn
   enddo
   
   ! copying yaux1 to the lower part yaux2
@@ -141,14 +174,15 @@ lwrite = .false.
   enddo
   
   ! calculates y on the lower side
-  do j = 1, jmid
+  do j = 1, jmid-1
     y(j) = -yaux2(j)
   enddo
-
+  
   ! calculates y on the upper side
-  do j = jmid+1, jmax
+  do j = jmid, jmax
     y(j) = yaux2(j)
   enddo
+
 !******************************************************
   ! 2) x direction calculation
 
@@ -196,7 +230,203 @@ lwrite = .false.
 return
 end subroutine mesh_baseflow
 
-!!###############################################################################
+!*******************************************************************************
+subroutine calc_eta
+
+ ! calculates eta variable as function of y and Reynolds number
+ 
+ implicit none
+ include '../var.f90'
+ include 'comm.mshbase'
+ 
+ integer(4):: j
+ real(8)   :: xpos
+ 
+ xpos = dabs(x(1))
+
+ !open (unit=1,file='eta.dat',status='unknown')
+  do j = 1,jmax
+    eta(j) = y(j)*dsqrt(Re/xpos)
+    !write(1,*) j, eta(j)
+  enddo
+ !close(1)
+
+return
+end subroutine calc_eta
+
+!*******************************************************************************
+
+!*******************************************************************************
+!subroutine calc_eta
+!
+!! calculates eta variable as function of y and Reynolds number
+!
+!implicit none
+!include '../var.f90'
+!include 'comm.mshbase'
+!
+!integer(4):: j
+!real(8):: ymax, etamax
+!
+!ymax = dabs(y(jmax))
+!etamax = 10.d0
+!
+!!open (unit=1,file='eta.dat',status='unknown')
+! do j = 1,jmax
+!   eta(j) = y(j)*etamax/ymax
+!   !write(1,*) j, eta(j)
+! enddo
+!!close(1)
+!
+!return
+!end subroutine calc_eta
+
+!*******************************************************************************
+subroutine integra
+implicit none
+include '../var.f90'
+include 'comm.varbase'
+include 'comm.mshbase'
+
+integer(4)          :: j, k, pt
+real(8),dimension(5):: f_eta
+real(8)             :: h
+ 
+ ! integration of the lower part
+ 
+ ! initial guesses
+ f_eta(1) = 0.d0
+ f_eta(2) = 0.76503626724495066d0 
+ f_eta(3) = 0.17283431626225954d0
+ f_eta(4) = 0.93109227001435113d0
+ f_eta(5) = 5.06285541989586352d-3
+ 
+ solu(:,jmid) = f_eta
+ 
+ do j = jmid - 1, 1, -1
+   h = eta(j) - eta(j+1)
+
+   pt = int ( dabs(h) / 1.d-4)
+   h = h / dfloat(pt)
+
+   do k = 1, pt
+     call rk4(f_eta, h)
+   enddo
+
+   solu(:,j) = f_eta
+
+ enddo
+ ! end of integration of the lower part
+
+ 
+ ! integration of the upper
+ 
+ ! initial guesses
+ f_eta(1) = 0.d0
+ f_eta(2) = 0.76503626724495066d0 
+ f_eta(3) = 0.17283431626225954d0
+ f_eta(4) = 0.93109227001435113d0
+ f_eta(5) = 5.06285541989586352d-3
+ 
+ do j = jmid + 1, jmax
+   h = eta(j) - eta(j-1)
+
+   pt = int ( dabs(h) / 1.d-4)
+   h = h / dfloat(pt)
+
+   do k = 1, pt
+     call rk4(f_eta, h)
+   enddo
+
+   solu(:,j) = f_eta
+ enddo
+ ! end of integration of the upper part
+
+ open (3, file='perfil.out',status='unknown')
+   write(3,*) 'j f f(1) f(2) g g(1) y eta'
+   do j = 1, jmax
+     write(3,*) j, solu(:,j), y(j), eta(j)
+   enddo
+ close(3)
+ 
+return
+end subroutine integra
+!*******************************************************************************
+subroutine rk4(f_eta, h)
+
+implicit none
+real(8), intent(inout) :: f_eta(5)
+real(8), intent(in)    :: h
+real(8)                :: hh, h6, dfd_eta(5), f_etat(5), dfd_eta2(5)
+
+   hh = h * 0.5d0
+   h6 = h / 6.d0
+
+   call derivs(f_eta, dfd_eta)
+   f_etat   = f_eta + hh * dfd_eta
+
+   call derivs(f_etat, dfd_eta2) 
+   f_etat   = f_eta + hh * dfd_eta2
+   dfd_eta = dfd_eta + 2.d0 * dfd_eta2
+
+   call derivs(f_etat, dfd_eta2)
+   f_etat   = f_eta + h * dfd_eta2
+   dfd_eta = dfd_eta + 2.d0 * dfd_eta2
+
+   call derivs(f_etat, dfd_eta2)
+   f_eta   = f_eta + h6 * ( dfd_eta + dfd_eta2 )
+
+ end subroutine rk4
+!*******************************************************************************
+subroutine derivs (f_eta, dfd_eta)
+  
+!  This subroutine sets the system of first order differential 
+!  equations for the compressible shear layer 
+!  
+!  ( Chi*f'' )' + f*f" = 0
+
+!  Momentum equations
+!  ( Chi*f'' )' + f*f" = 0 
+!   y(1)  = f
+!   y(2)  = f'
+!   y(3)  = f''
+!   dy(3) = f'''
+
+!  Energy equations
+!   (( Chi/Pr)*g' )' + f*g' + Chi*(u_e^2/h_e)*( f'' )^2 = 0
+!   y(4)  = g
+!   y(5)  = g'
+!   dy(5) = g''
+
+! Chi = 2.0
+
+implicit none
+real(8), intent(in)    :: f_eta(5)
+real(8), intent(out)   :: dfd_eta(5)
+real(8)                :: Pr, U1, h1, Chi
+
+ Pr = 0.66465d0
+ U1 = 155.83d0
+ h1 = -9444.04955d0
+ Chi = 1.d0
+ 
+ dfd_eta(1) =   f_eta(2)
+ dfd_eta(2) =   f_eta(3)
+ dfd_eta(3) = - ( f_eta(1) * f_eta(3) )/ Chi
+ dfd_eta(4) =   f_eta(5)
+ dfd_eta(5) = - (Pr * f_eta(1) * f_eta(5))/Chi - (Pr * U1 * U1 * f_eta(3) * f_eta(3) / h1)
+
+ 
+ !dfd_eta(1) =   f_eta(2)
+ !dfd_eta(2) =   f_eta(3)
+ !dfd_eta(3) = - 0.5d0 * f_eta(1) * f_eta(3)
+ !dfd_eta(4) =   f_eta(5)
+ !dfd_eta(5) = - 0.5d0 * Pr * f_eta(1) * f_eta(5) - Pr * U1 * U1 * f_eta(3) * f_eta(3) / h1
+
+
+end subroutine derivs
+!*******************************************************************************
+
 ! subroutine escreve(itime)
 ! implicit none
 ! include 'var.f90'
@@ -237,4 +467,4 @@ end subroutine mesh_baseflow
 ! 
 ! return
 ! end subroutine
-!!###############################################################################
+!!*******************************************************************************
---
+++ var.f90
@@ -0,0 +1,96 @@
+integer(4):: nprocx, nprocy, lvc, mp, ncomu, ivar, inter, interderv, interfilt, imaxin, jmaxin, ptsx, ptsy, &
+             imax, jmax, iteramax, nmodes, npdamp, npfilter, icomp
+
+logical:: restart, ldisturb, lprintform, lfilter
+
+real(8):: dt, tfinal
+real(8):: dksi, dn, stx, sty, xl, yl, posxin, posyin
+real(8):: mach, Re, Pr, cv, kappa, kcond, mi
+real(8):: af, bf, cf, df, alphaf
+real(8):: omega, t_period
+real(8):: damp, pi
+
+parameter (restart  = .false.)   ! se reinicia ou não a simulação a partir de uma anterior
+parameter (lprintform = .true.) ! se imprime formatado ou nao
+
+parameter (nprocx = 1)  ! numero de sub-dominios na direcao x
+parameter (nprocy = 1)  ! numero de sub-dominios na direcao y
+parameter (lvc = 1)     ! numero de niveis do ciclo v
+parameter (mp = 5)     ! numero de pontos da molecula computacional necessaria para 
+                       ! o cálculo das derivadas
+parameter (ncomu =  3) ! numero de cominicacoes na volta do pipeline
+parameter (ivar  =  4) ! numero de dimensoes da variavel Q
+parameter (icomp  = 2500) ! intervalo de passos de tempo para fazer comparação
+
+!Definindo o valor de inter (numero de volumes no overlap de cada malha)
+parameter(interderv = 2**(lvc-1)*(mp-2))
+parameter(interfilt = 2) ! o +2 ocorre porque o filtro precisa de 2 pontos adicionais
+parameter(inter = interderv + interfilt)
+
+!parameter (imaxin = 61)  
+!parameter (jmaxin = imaxin)
+!parameter (imaxin = 2500, jmaxin=850)  ! Malha Quali
+parameter (imaxin = 2500, jmaxin=200)  ! Malha Quali
+
+! Definindo numero de pontos para cada nó
+parameter (ptsx = (imaxin+(inter+1)*(nprocx-1))/nprocx)
+parameter (ptsy = (jmaxin+(inter+1)*(nprocy-1))/nprocy)
+
+!Total de número de pontos em cada direcao somando todos os nós
+parameter (imax = ptsx*nprocx -(inter+1)*(nprocx-1), jmax = ptsy*nprocy -(inter+1)*(nprocy-1))
+
+parameter (dksi     = 0.157d0)              !tamanho do delta na direcao ksi
+parameter (dn       = 0.149998305009144d0)  !tamanho do delta na direcao n
+
+! Para MMS ------------------------------------------------------------------
+!parameter (pi       = dacos(-1.d0))
+!parameter (xl       =  2.d0*pi )
+!parameter (yl       =  2.d0*pi )
+!parameter (posxin   =  -pi + pi/16.d0)
+!parameter (posyin   =  -pi + pi/16.d0 )
+!parameter (dksi     = xl/dfloat(imax-1))  !tamanho do delta na direcao ksi
+!parameter (dn       = yl/dfloat(jmax-1))  !tamanho do delta na direcao n
+!!modificado para o teste de ordem com stretching
+!parameter (stx      = 1.d0)
+!parameter (sty      = 1.d0)
+!----------------------------------------------------------------------------
+
+parameter (mach     = 0.1d0)
+parameter (Re       = 250.d0)
+parameter (Pr       = 100.d0)
+parameter (cv       = 0.1d0)
+parameter (mi       = 0.3d0)
+parameter (kcond    = 1.d0)
+parameter (kappa    = 1.4d0 )
+parameter (dt       = 1.d-10 )
+parameter (tfinal   = 5.d-9 )
+parameter (iteramax = dint(tfinal/dt)+1)
+
+!constantes para MMS
+!real(8):: rho0, u0, v0, et0, omg, eps
+!parameter(rho0 = 1.d0)
+!parameter(  u0 = 0.1d0)
+!parameter(  v0 = 1.0d0)
+!parameter( et0 = 0.5d0)
+!parameter( omg = 1.0d0)
+!parameter( eps = 0.5d0)
+
+!Parametros para o filtro
+!Filter constants (Lele C.2.5)
+parameter ( lfilter = .false. )
+parameter ( npfilter = 5)
+parameter ( alphaf = 0.48d0 )
+parameter ( af = (11.d0+10.d0*alphaf)/16.d0 )
+parameter ( bf = (15.d0+34.d0*alphaf)/64.d0 ) !/2
+parameter ( cf = (-3.d0+ 6.d0*alphaf)/32.d0 ) !/2
+parameter ( df = ( 1.d0- 2.d0*alphaf)/64.d0 ) !/2
+
+! Parametros para o damping na direcao y
+parameter ( npdamp = 40    ) ! numero de pontos no amortecimento
+parameter ( damp   = 1.d-2 ) ! fator de amortecimento
+
+!Variáveis para excitacao do LST
+parameter (ldisturb = .false.)  ! se calcula ou nao as perturbaçoes dadas pela LST e as características
+parameter (nmodes   = 4)        ! número de modos que serao excitados
+parameter (omega    = 1.d0)     ! freq de excitacao
+parameter (t_period = 1.d0)     ! period
